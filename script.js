16eaa26d123a572d3c8fc6bd9c7a32433f5662cd
вика <nesterovav888@gmail.com>
2022-05-10 13:51:53 +0300

    JavaScript темы : файлы со скриптами, кеширование, строгий режим, комментарий, переменные.

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+/*alert('ПРИВЕТ');
+let num=123;
+alert(num);
+let a;
+
+a=1;
+alert(a);
+
+a = 2;
+alert(a);
+let a = 1 + 2 +3;
+alert(a);
+let a = 10;
+let b = 5;
+let c = a + b;
+alert(c);
+*/
